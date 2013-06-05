0952f9feff30621d7ef4b6db2c782575c28d7df7
Jonathan Tamboer <jonathanlemuel@me.com>
2013-06-05 11:54:05 -0400

    Add comments and clean up.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,20 @@
 class CommentsController < ApplicationController
 
   def index
+    @comment = Comment.new
     @comments = Comment.order("id DESC").all
   end
 
   def create
     @comment = Comment.new(params[:comment])
+    @comment.save
 
-    if @comment.save
-      if request.xhr?
-        render partial: "comment", locals: {comment: @comment}
-      else
-        redirect_to comments_url
-      end
+    if request.xhr?
+      # Render a partial as response when using ajax requests.
+      render partial: "comment", locals: {comment: @comment}
     else
-      render action: "new"
+      # Redirect as usual for plain html requests.
+      redirect_to comments_url
     end
   end
 end
